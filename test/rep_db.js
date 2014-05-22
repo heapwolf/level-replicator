a8a9e4ed321f259b6031159e563c85c0f7874174
Jason Smith (work) <jason.h.smith@gmail.com>
2014-05-22 07:05:32 +0000

    Move utility/debugging code to its own file

--- test/rep_db.js
+++ test/rep_db.js
@@ -37,16 +37,3 @@ describe('Replicator DB', function () {
     })
   })
 })
-
-// Utility function for debugging
-function dir(db, name) {
-  var results = []
-  db.createReadStream()
-  .on('data', function(D) { results.push(D) })
-  .on('end', function() {
-    console.log('=-=-=-=-= %s', name || '')
-    for (var i = 0; i < results.length; i++)
-      console.log('%s = %j', results[i].key, results[i].value)
-    console.log('=-=-=-=-=')
-  })
-}
